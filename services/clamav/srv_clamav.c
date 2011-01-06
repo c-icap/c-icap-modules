ce7836f88c9ac0704c663ea0425f16453fc7dbf3
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2011-01-06 09:30:49 +0000

    Bug fix: #3146990 The SendPercentData can not set to 0
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/branches/c_icap_modules_0_1_x@705 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- services/clamav/srv_clamav.c
+++ services/clamav/srv_clamav.c
@@ -1047,7 +1047,7 @@ int cfg_SendPercentBytes(char *directive, char **argv, void *setdata)
 
      SEND_PERCENT_BYTES = val;
      ci_debug_printf(2, "Setting parameter :%s=%d\n", directive, val);
-     return val;
+     return 1;
 }
 
 
