9a6d1932f41d8b40ccd888fac95549ae621c736c
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2011-05-23 13:23:39 +0000

    Reverting the "Bug fix: sg_db_t struct does not initialized properly in sg_init_db function" patch. It was not valid for 0.1.x version
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/branches/c_icap_modules_0_1_x@777 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- services/url_check/sguardDB.c
+++ services/url_check/sguardDB.c
@@ -185,8 +185,6 @@ sg_db_t *sg_init_db(char *home)
     sg_db->env_db=NULL;
     sg_db->domains_db=NULL;
     sg_db->urls_db=NULL;
-    sg_db->domains_db_name = NULL;
-    sg_db->urls_db_name = NULL;
 
     sg_db->env_db = db_setup(home);
     if(sg_db->env_db==NULL){
